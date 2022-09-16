5b0c7f75712b818341bf020b70aae1ba4012dfc9
Stanislav Agafonov <72259678+AgafonovChita@users.noreply.github.com>
2022-09-16 19:12:34 +0900

    Delete config.py

--- bot/config.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class Config:
-    bot_token: str = "5759693042:AAHCTi4GbWqQdS5ZoAW2kAE6WkT-wRFJ4P0"
-    db_url: str = 'sqlite+aiosqlite:///travel_game.db'
